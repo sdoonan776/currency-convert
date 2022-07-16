f6ca88dfaa16c9f7b8c3e15aa270aa658b30ecc4
Stu Doonan <studoonan@yahoo.co.uk>
2022-07-17 00:33:08 +0100

    created components, added react bootstrap, started working on currency conversion logic

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, {ReactElement} from 'react';
+
+function Footer(): ReactElement {
+    return (
+        <>
+            <footer>
+                 
+            </footer>
+        </>
+    ); 
+}
+
+export default Footer;
